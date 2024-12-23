fa3953bcd9abf87171c30ca815899b8e80d870e2
JcdbTPU40 <g2427040@st.t-kougei.ac.jp>
2024-12-23 14:12:57 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,33 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+    if (begin == NULL || end == NULL) {
+        return false;
+    }
+
+    if (end <= begin) {
+        return false;
+    }
+
+    const size_t size = end - begin;
+    if (size < 2) {
+        return true;
+    }
+
+    for (size_t i = 0; i < size - 1; i++) {
+        bool swapped = false;
+        for (size_t j = 0; j < size - 1 - i; j++) {
+            if (begin[j].key > begin[j + 1].key) {
+
+                item temp = begin[j];
+                begin[j] = begin[j + 1];
+                begin[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+    return true;
 }
